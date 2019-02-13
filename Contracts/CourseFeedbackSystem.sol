0c6cd95909a39a74ac44e9ddd53084809fd9d935
shubhgsgarg <39940156+shubhgsgarg@users.noreply.github.com>
2019-02-13 20:26:15 +0530

    Update CourseFeedbackSystem.sol

--- Contracts/CourseFeedbackSystem.sol
+++ Contracts/CourseFeedbackSystem.sol
@@ -6,22 +6,22 @@ contract CourseFeedback{
 
   // Store students Count
   uint public studentsCount;
-
   //Store number of students who have voted till now
   uint numberOfVotedStudents=0;
-
   // Store admin credentials
   address admin;
-  
-  modifier onlyAdmin() { 
-    require (msg.sender==admin); 
-    _; 
+
+  modifier onlyAdmin() {
+    require (msg.sender==admin);
+    _;
   }
-  
+
+
   //structure to store candidate's details
   struct Student{
     uint studentID;
     string studentname;
+    bool validStud;
     //feedback
   }
 
@@ -29,20 +29,18 @@ contract CourseFeedback{
   struct Feedback {
     //Question variables go here
   }
-  
+
 
   //Mappings
   // mapping of all the students :studentsList
   mapping (address=>Student) studentsList;
-
   // mapping of hashes of pk/password with public key :passDB
-  mapping ( => ) passDB;
-
+  //mapping ( => ) passDB;
   //voted
   mapping (address=>bool) public voted;
-
   //mapping to store feedback
   mapping ( address => Feedback) public feedbackRecord;
+
 
   //constructor
   constructor() public {
@@ -50,31 +48,34 @@ contract CourseFeedback{
     admin=msg.sender;
   }
 
+
   function setNumberOfStudents (uint numberOfStudents) onlyAdmin {
     //sets the number of students allowed to give reviews
     studentsCount=numberOfStudents;
   }
-  
+
+
   //add students
   function addStudent (uint _studentid,string _studentname) onlyAdmin public {
     //making a new student struct
-    Student temp=Student(_studentid,_studentname);
+    Student temp=Student(_studentid,_studentname,true);
     //add to studentsList
-    
+
     //update passDB
     //increase numberOfVotedStudents
     numberOfVotedStudents++;
   }
-  
+
 
 
   //give feedback
   function giveFeedback (int a, int b) public {
       // requires that they are in studentsList
+      require(studentsList[msg.sender].validStud=true);
       // requires they have pk/password
       // requires that they haven't given feedback before
       require(voted[msg.sender]==false);
-      
+
       //add to feedbackRecord
       Feedback temp = Feedback(a,b);
       feedbackRecord[msg.sender] = temp;
@@ -82,13 +83,14 @@ contract CourseFeedback{
       voted[msg.sender]=true;
   }
 
+
   // check feedback
   function checkFeedback() public view returns (Feedback){
       //chekc if the student voted
       require(voted[msg.sender]== true);
       //check feedbackRecord
       return feedbackRecord[msg.sender];
-      
+
   }
 
 
